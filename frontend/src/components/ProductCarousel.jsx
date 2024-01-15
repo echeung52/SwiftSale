99c5cd7484c10ece75294e58e4415d58491cf63d
Eric <58400036+echeung52@users.noreply.github.com>
2024-01-14 21:37:26 -0500

    carousel fix

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -21,21 +21,19 @@ export default function ProductCarousel() {
   ) : error ? (
     <Alert variant="danger">{error}</Alert>
   ) : (
-    !products && (
-      <Carousel pause="hover" className="bg-dark">
-        {products.map((product) => (
-          <Carousel.Item key={product._id}>
-            <Link to={`/product/${product._id}`}>
-              <Image src={product.image} alt={product.name} />
-              <Carousel.Caption className="carousel.caption">
-                <h4>
-                  {product.name}(${product.price})
-                </h4>
-              </Carousel.Caption>
-            </Link>
-          </Carousel.Item>
-        ))}
-      </Carousel>
-    )
+    <Carousel pause="hover" className="bg-dark">
+      {products.map((product) => (
+        <Carousel.Item key={product._id}>
+          <Link to={`/product/${product._id}`}>
+            <Image src={product.image} alt={product.name} />
+            <Carousel.Caption className="carousel.caption">
+              <h4>
+                {product.name}(${product.price})
+              </h4>
+            </Carousel.Caption>
+          </Link>
+        </Carousel.Item>
+      ))}
+    </Carousel>
   );
 }
