eac5b2e105aa5fc175836f3ed405059194f73232
Eric <58400036+echeung52@users.noreply.github.com>
2024-01-14 22:01:53 -0500

    carousel

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -25,7 +25,7 @@ export default function ProductCarousel() {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image src={product.image} alt={product.name} />
+            <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className="carousel.caption">
               <h4>
                 {product.name}(${product.price})
